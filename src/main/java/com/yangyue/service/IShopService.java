ff69c63b57f3ba32b84e5d7494b6ff77f9d46dc5
yangyue-l <1695847539@qq.com>
2026-05-11 22:36:41 +0800

    利用Redis中的GEO实现查看附近店铺功能

--- src/main/java/com/yangyue/service/IShopService.java
+++ src/main/java/com/yangyue/service/IShopService.java
@@ -21,4 +21,6 @@ public interface IShopService extends IService<Shop> {
 
     Result update(Shop shop);
 
+    Result queryShopByType(Integer typeId, Integer current, Double x, Double y);
+
 }
--- src/main/java/com/yangyue/service/impl/ShopServiceImpl.java
+++ src/main/java/com/yangyue/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.yangyue.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yangyue.dto.Result;
 import com.yangyue.entity.Shop;
@@ -7,13 +8,26 @@ import com.yangyue.mapper.ShopMapper;
 import com.yangyue.service.IShopService;
 import com.yangyue.utils.CacheClient;
 import com.yangyue.utils.RedisConstants;
+import com.yangyue.utils.SystemConstants;
 
+import cn.hutool.core.util.StrUtil;
 
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.connection.RedisGeoCommands.GeoLocation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Resource;
 
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResult;
+import org.springframework.data.geo.GeoResults;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.domain.geo.GeoReference;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -93,6 +107,59 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         //删除缓存
         stringRedisTemplate.delete(RedisConstants.CACHE_SHOP_KEY + id);
         return Result.ok();
+    }
+
+
+    @Override
+    public Result queryShopByType(Integer typeId, Integer current, Double x, Double y) {
+        //判断是否需要根据坐标查询
+        if(x == null || y == null){
+            // 根据类型分页查询
+            Page<Shop> page = query()
+                    .eq("type_id", typeId)
+                    .page(new Page<>(current, SystemConstants.DEFAULT_PAGE_SIZE));
+            // 返回数据
+            return Result.ok(page.getRecords());
+        }
+        //计算分也参数
+        int from = (current - 1) * SystemConstants.DEFAULT_PAGE_SIZE;
+        int end = current * SystemConstants.DEFAULT_PAGE_SIZE;
+
+        String key = RedisConstants.SHOP_GEO_KEY + typeId;
+        GeoResults<GeoLocation<String>> results = stringRedisTemplate.opsForGeo()
+                                    .search(key,
+                                         GeoReference.fromCoordinate(x, y),
+                                         new Distance(5000),
+                                         RedisGeoCommands.GeoSearchCommandArgs.newGeoSearchArgs().includeDistance().limit(end));
+
+        if(results == null){
+            return Result.ok(Collections.emptyList());
+        }
+        List<GeoResult<GeoLocation<String>>> list = results.getContent();
+
+        if(list.size() <= from){
+            return Result.ok(Collections.EMPTY_LIST);
+        }
+
+        List<Long> ids = new ArrayList<>(list.size());
+        HashMap<String,Distance> distanceMap = new HashMap<>(list.size());
+
+        list.stream().skip(from).forEach(result -> {
+            String shopIdStr = result.getContent().getName();
+            ids.add(Long.valueOf(shopIdStr));
+            Distance distance = result.getDistance();
+            distanceMap.put(shopIdStr, distance);
+        });
+
+        String idStr = StrUtil.join(",", ids);
+        List<Shop> shops = query().in("id", ids).last("ORDER BY FIELD(id," + idStr + ")").list();
+
+        for(Shop shop : shops){
+            shop.setDistance(distanceMap.get(shop.getId().toString()).getValue());
+        }
+
+
+        return Result.ok(shops);
     }
 
 }
